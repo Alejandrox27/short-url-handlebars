995433f687527c9cb8e90a7cae8fd2b0670c6caf
alx_27 <didieralejandromejia83@gmail.com>
2023-12-26 18:24:44 -0500

    create public dir and middleware in index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", (req, res) => {
+    res.send("estas solicitando la ruta raiz")
+})
+
 app.listen(5000, () => console.log("server on"))
