33acae722615a28bf8b14332272973bd902cb62e
alx_27 <didieralejandromejia83@gmail.com>
2023-12-26 18:51:46 -0500

    templates with hbs

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express")
+const { create } = require("express-handlebars");
+
 const app = express()
 
-app.use(express.static(__dirname + "/public"))
+const hbs = create({
+    extname: ".hbs",
+});
+
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
 
 app.get("/", (req, res) => {
-    res.send("estas solicitando la ruta raiz")
+    res.render("home")
 })
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.use(express.static(__dirname + "/public"))
 
 app.listen(5000, () => console.log("server on"))
