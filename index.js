c665bda3d7489ff2f0411a37bf83dd58731add3c
alx_27 <didieralejandromejia83@gmail.com>
2023-12-26 19:13:12 -0500

    home.hbs template card

--- index.js
+++ index.js
@@ -13,13 +13,14 @@ app.set("views", "./views");
 
 
 app.get("/", (req, res) => {
-    res.render("home")
+    
+    res.render("home");
 })
 
 app.get("/login", (req, res) => {
-    res.render("login")
+    res.render("login");
 })
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 
-app.listen(5000, () => console.log("server on"))
+app.listen(5000, () => console.log("server on"));
