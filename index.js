9a4586044d6443a1006d1d7400b785489661f498
alx_27 <didieralejandromejia83@gmail.com>
2023-12-26 21:00:11 -0500

    Some changes

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const app = express()
 
 const hbs = create({
     extname: ".hbs",
-    partialsDir: ["views/components"],
+    partialsDir: ["views/components"], //partial dirs (components)
 });
 
 app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 
+//Routers
+
 app.use("/", require("./routes/home"))
 app.use("/auth", require("./routes/auth"))
+
+//Show interface from /public with express static
 
 app.use(express.static(__dirname + "/public"));
 
