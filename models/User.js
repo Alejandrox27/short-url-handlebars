a72eddfd717161a5ec1c25517d9a088aa704e8cb
alx_27 <didieralejandromejia83@gmail.com>
2023-12-28 16:39:54 -0500

    Schema for User

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        lowercase: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        index: {unique: true}
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    tokenConfirm: {
+        type: String,
+        default: null
+    },
+    confirmAccount: {
+        type: Boolean,
+        default: false
+    }
+})
